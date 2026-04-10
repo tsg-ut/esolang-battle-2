75b2a9c9c6f13791b30b69f4577da8a2b5a112f7
Taisei-dev <87258462+Taisei-dev@users.noreply.github.com>
2026-04-10 14:43:17 +0900

    remove unnecessary backend and frontend

--- apps/backend/function/getLanguages.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from "@esolang-battle/db";
-
-export async function getLanguages(prisma: PrismaClient) {
-  const languages = await prisma.language.findMany({
-    orderBy: { id: "asc" },
-  });
-
-  return languages;
-}
